aece94f4a4b99cbf8fbc7611695a445be6666f6f
yudanZ <zhai.yudan@gmail.com>
2020-10-08 11:12:36 +0200

    Updates

---
+++ precache-manifest.dec95aa3828c1570fd6c2bd83c7aecf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e70ae1e21a02dca1fae4e5460c1e686",
+    "url": "/disney-characters/index.html"
+  },
+  {
+    "revision": "b28165711a85769d36a9",
+    "url": "/disney-characters/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7d3f58715fad2c827352",
+    "url": "/disney-characters/static/css/main.53ee7564.chunk.css"
+  },
+  {
+    "revision": "b28165711a85769d36a9",
+    "url": "/disney-characters/static/js/2.993c3764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/disney-characters/static/js/2.993c3764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3f58715fad2c827352",
+    "url": "/disney-characters/static/js/main.eab1267a.chunk.js"
+  },
+  {
+    "revision": "477223a7cdda09f51e40",
+    "url": "/disney-characters/static/js/runtime-main.2c453e7e.js"
+  }
+]);
