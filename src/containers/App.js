92f460c76fa5f58ddb3267b340cda0d203077eac
yudanZ <zhai.yudan@gmail.com>
2020-10-19 13:58:12 +0200

    rewriting code by using Hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,95 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import CartList from '../components/cartList.component';
 import SearchBox from '../components/searchBox.component';
-import { apiCall } from '../api/api';
-import './App.css';
-//import Scroll from '../components/scroll.component';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import ErrorBoundry from '../components/errorBoundry.component';
-class App extends React.Component {
-  constructor(props){
-    super();
+import {apiCall} from '../api/api';
 
-    this.state = {
-      characters: [],
-      searchField: '',
-      page: 1,
-      loaded: false,
-      hasMore: true
+import './App.css';
+
+import ErrorBoundry from '../components/errorBoundry.component';
+
+function App () {
+
+  const [characters, setCharacters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [page, setPage] = useState(1);
+  const [loadMore, setLoadMore] = useState(false);
+  const [hasMore, setHasMore] = useState( true);
+  
+  
+  
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
+  useEffect(() => {
+    setLoadMore(false);
+    getCharacters();
+    
+  }, [loadMore]);
+
+  const handleScroll = () => {
+    const scrollTop = (document.documentElement
+      && document.documentElement.scrollTop)
+      || document.body.scrollTop;
+    const scrollHeight = (document.documentElement
+      && document.documentElement.scrollHeight)
+      || document.body.scrollHeight;
+    if (scrollTop + window.innerHeight + 50 >= scrollHeight){
+      setLoadMore(true);
+    }
+  }
+
+  const getCharacters = () => {
+    if(hasMore){
+      apiCall(`https://api.disneyapi.dev/characters?page=${page}`)
+      .then(data => {
+        //console.log(data)
+        const filterData = data.data.filter( item => item.imageUrl !== undefined)
+        setCharacters(filterData);
+        if( data.nextPage){
+          
+          setPage(page + 1);
+          setHasMore(true);
+        }else{
+          setHasMore(false);
+         
+        }
+        
+        //console.log( this.state)
+      }) 
+      setLoadMore(false);
     }
     
   }
 
-  componentDidMount(){
-    this.getCharacters();
-    
-  }
-
-  
-  getCharacters = () => {
-
-    let { page } = this.state;
-    apiCall(`https://api.disneyapi.dev/characters?page=${page}`)
-    .then(data => {
-      //console.log(data.data)
-      const filterData = data.data.filter( item => item.imageUrl !== undefined)
-      this.setState({characters: filterData});
-      this.setState({loaded: true});
-      if( data.nextPage){
-        page ++
-        this.setState({page: page});
-        this.setState({hasMore: true});
-      }else{
-        this.setState({hasMore: false})
-      }
-
-      //console.log( this.state)
-    })
-    
-  }
-
-  onSearchChange = (event) => {
+  const onSearchChange = (event) => {
     //console.log(event.target.value);
-    this.setState({searchField: event.target.value})
+    //this.setState({searchField: event.target.value})
+    setSearchField(event.target.value);
   }
 
-  render(){
-    const { characters, searchField} = this.state;
-
+  const filterCharacters = (characters, searchField) => {
     const filterCharacters = characters.filter( character => { 
       return  character.name.toLowerCase().includes(searchField.toLowerCase())
     })
 
+    return filterCharacters;
+  }
+  
+  
     return !characters.length ?
       <h1>Loading</h1>
       :
       (
         <div className='tc'>
           <h1 className="f2">Disney Characters</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <InfiniteScroll
-            dataLength={filterCharacters}
-            next={() => this.getCharacters()}
-            hasMore={this.state.hasMore}
-            loader={
-              <img
-                src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
-                alt="loading"
-              />
-            }>
-            {this.state.loaded ?
+          
+          <SearchBox searchChange={onSearchChange}/>
+          
+            {hasMore ?
               <ErrorBoundry>
-                <CartList characters= {filterCharacters}/>
+                <CartList characters= {filterCharacters(characters, searchField)}/>
               </ErrorBoundry>
               :
-              ''
+              <div><img
+              src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+              alt="loading"
+            /></div>
             }
             
-          </InfiniteScroll>
         </div>          
       )     
-  } //render
+  //render
 }
-
-export default App;
+export default App
